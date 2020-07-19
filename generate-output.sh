99a0b607c424eafb15467890b32586d13c9828d6
Leo Lamprecht <mindrun@icloud.com>
2020-07-19 18:48:53 +0200

    Update generate-output.sh

--- generate-output.sh
+++ generate-output.sh
@@ -1,4 +1,8 @@
 #! /bin/bash
-for n in {1..1000}; do
+
+mkdir public
+cd public
+
+for n in {1..17000}; do
     dd if=/dev/urandom of=file$( printf %03d "$n" ).bin bs=1 count=$(( RANDOM + 1024 ))
 done
